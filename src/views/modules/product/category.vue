2b1c580d1af893a57230a4be636beee01c52244c
AnZaiyun <1732478192@qq.com>
2020-11-11 09:08:31 +0800

    first commit

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <el-tree
+      :data="data"
+      :props="defaultProps"
+      @node-click="handleNodeClick"
+    ></el-tree>
+  </div>
+</template>
+
+<script>
+// 这里可以导入其他文件，如组件、工具js、第三方插件js、json文件、图片文件等
+// 例如：import 《组件名称》 from 《组件路径》
+
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      data: [],
+      defaultProps: {
+        children: "children",
+        label: "label",
+      },
+    };
+  },
+  // 计算属性 类似data概念
+  computed: {},
+  // 监控data中的数据变化
+  watch: {},
+  // 方法集合
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
+    getMenus(){
+        this.$http({
+          url: this.$http.adornUrl('/product/category/list/tree'),
+          method: 'get'
+        }).then(({data}) => {
+          console.log("sucess")
+        })
+    }
+  },
+  created() { this.getMenus()},
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  activated() {
+     
+  },
+};
+</script>
+
+<style scoped>
+</style>
