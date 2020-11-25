fcb1ecee9ab036c07e1d0b37eed25e7529d81498
AnZaiyun <1732478192@qq.com>
2020-11-25 13:49:38 +0800

    文件上传到阿里云存储功能实现

--- src/views/modules/product/brand-add-or-update.vue
+++ src/views/modules/product/brand-add-or-update.vue
@@ -5,13 +5,14 @@
                 <el-input v-model="dataForm.name" placeholder="品牌名"></el-input>
             </el-form-item>
             <el-form-item label="品牌logo地址" prop="logo">
-                <el-input v-model="dataForm.logo" placeholder="品牌logo地址"></el-input>
+                <!-- <el-input v-model="dataForm.logo" placeholder="品牌logo地址"></el-input> -->
+                <single-upload v-model="dataForm.logo"></single-upload>
             </el-form-item>
             <el-form-item label="介绍" prop="descript">
                 <el-input v-model="dataForm.descript" placeholder="介绍"></el-input>
             </el-form-item>
             <el-form-item label="显示状态" prop="showStatus">
-                <el-switch v-model="dataForm.showStatus" active-color="#13ce66" inactive-color="#ff4949">
+                <el-switch v-model="dataForm.showStatus" active-color="#13ce66" inactive-color="#ff4949" :active-value="1" :inactive-value="0">
                 </el-switch>
             </el-form-item>
             <el-form-item label="检索首字母" prop="firstLetter">
@@ -29,7 +30,9 @@
 </template>
 
 <script>
+import SingleUpload from '@/components/upload/singleUpload'
 export default {
+    components: {SingleUpload},
     data() {
         return {
             visible: false,
@@ -38,7 +41,7 @@ export default {
                 name: '',
                 logo: '',
                 descript: '',
-                showStatus: '',
+                showStatus: 1,
                 firstLetter: '',
                 sort: ''
             },
